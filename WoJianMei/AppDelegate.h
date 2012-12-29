48c7dc3c14fffcf243af8a0c5fc6a73c28b03688
Tom Callon <tomcallon2010@gmail.com>
2012-12-30 00:37:05 +0800

    commit

---
+++ WoJianMei/AppDelegate.h
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.h
+//  WoJianMei
+//
+//  Created by Tom Callon on 7/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+#import "SinaweiboManager.h"
+
+@class TengxunWeiboManager;
+@class PPTabBarController;
+
+
+enum
+{
+    TAB_REALTIME_SCORE = 0,
+};
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,UITabBarControllerDelegate,SinaWeiboDelegate>
+
+{
+      PPTabBarController	*_tabBarController;
+      UINavigationController *_navigationController;
+    
+      SinaweiboManager *sinaWeiboManager;
+      TengxunWeiboManager *tengxunWeiboManager;
+
+
+}
+
+@property (strong, nonatomic) UIWindow *window;
+@property (readonly, nonatomic) SinaweiboManager *sinaWeiboManager;
+@property (readonly,nonatomic)  TengxunWeiboManager *tengxunWeiboManager;
+@property (nonatomic,retain) UINavigationController *navigationController;
+
+
+@property (nonatomic, retain)   PPTabBarController	*tabBarController;
+
+
+@end
---
+++ WoJianMei/AppDelegate.m
@@ -0,0 +1,244 @@
+//
+//  AppDelegate.m
+//  WoJianMei
+//
+//  Created by Tom Callon on 7/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "PPTabBarController.h"
+
+
+#import "WorkoutViewController.h"
+#import "MyselfViewController.h"
+#import "Nutri_SupViewController.h"
+#import "MakeFriendsViewController.h"
+#import "MoreViewController.h"
+
+
+
+#import "PlayVideosViewController.h"
+
+
+#import "SettingsViewController.h"
+#import "MySideMenueViewController.h"
+#import "MFSideMenu.h"
+
+#import "UIUtils.h"
+
+#import "SinaweiboManager.h"
+
+
+
+#define kAppRedirectURI     @"http://aijianmei.com"
+
+
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+@synthesize sinaWeiboManager;
+@synthesize tengxunWeiboManager;
+@synthesize navigationController =_navigationController;
+@synthesize tabBarController=_tabBarController ;
+
+
+
+- (void)dealloc
+{
+    [_tabBarController release];
+    [sinaWeiboManager release];
+    [tengxunWeiboManager release];
+    [_navigationController release];
+    [_window release];
+    [super dealloc];
+}
+
+- (void)initTabViewControllers
+{
+    
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard_iPhone" bundle:nil];
+    PPTabBarController *tbController = [storyboard instantiateViewControllerWithIdentifier:@"PPTabBarController"];
+
+    
+    self.tabBarController = tbController;
+    
+    self.tabBarController.delegate = self;
+    
+    
+
+	NSMutableArray* controllers = [[NSMutableArray alloc] init];
+    
+	WorkoutViewController* workoutViewController = (WorkoutViewController*)[storyboard instantiateViewControllerWithIdentifier:@"WorkoutViewController"];
+    [UIUtils addViewControllerFromStoryBoard: workoutViewController
+					 viewTitle:@""
+					 viewImage:@"b_menu_1.png"
+			  hasNavController:NO
+			   viewControllers:controllers];
+    
+    Nutri_SupViewController * nutri_SupViewController = (Nutri_SupViewController*)[storyboard instantiateViewControllerWithIdentifier:@"Nutri_SupViewController"];
+    [UIUtils addViewControllerFromStoryBoard:nutri_SupViewController
+                     viewTitle:@""
+                     viewImage:@"b_menu_2.png"
+              hasNavController:NO
+               viewControllers:controllers];
+
+    MyselfViewController *myselfViewController = (MyselfViewController *)[storyboard instantiateViewControllerWithIdentifier:@"MyselfViewController"];
+    [UIUtils addViewControllerFromStoryBoard:myselfViewController
+                                   viewTitle:@""
+                                   viewImage:@"b_menu_3.png"
+                            hasNavController:NO
+                             viewControllers:controllers];
+
+    
+    MakeFriendsViewController *makeFriendsViewController =(MakeFriendsViewController *)[storyboard instantiateViewControllerWithIdentifier:@"MakeFriendsViewController"];
+    
+    
+	[UIUtils addViewControllerFromStoryBoard:makeFriendsViewController
+					 viewTitle:@""
+					 viewImage:@"b_menu_4.png"
+			  hasNavController:NO
+			   viewControllers:controllers];
+    
+    
+    MoreViewController *moreViewController =(MoreViewController *)[storyboard instantiateViewControllerWithIdentifier:@"MoreViewController"];
+	[UIUtils addViewControllerFromStoryBoard:moreViewController
+                                   viewTitle:@""
+                                   viewImage:@"b_menu_5.png"
+                            hasNavController:NO
+                             viewControllers:controllers];
+    
+    
+    self.tabBarController.viewControllers = controllers;
+
+    
+    [self.tabBarController setSelectedImageArray:[NSArray arrayWithObjects:
+                                                  @"b_menu_1s.png",
+                                                  @"b_menu_2s.png",
+                                                  @"b_menu_3s.png",
+                                                  @"b_menu_4s.png",
+                                                  @"b_menu_5s.png",
+                                                  nil]];
+    
+    self.tabBarController.selectedIndex = TAB_REALTIME_SCORE;
+    
+   [controllers release];
+    
+    
+}
+
+
+- (void)customizeInterface
+{
+    
+    UIImage* tabBarBackground = [UIImage imageNamed:@"tabbar.png"];
+    [[UITabBar appearance] setBackgroundImage:tabBarBackground];
+    [[UITabBar appearance] setSelectionIndicatorImage:[UIImage imageNamed:@"selection-tab.png"]];
+    
+}
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    
+    self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+
+    [[UIApplication sharedApplication] setStatusBarHidden:NO];
+    
+    
+   [self customizeInterface];
+   [self initTabViewControllers];
+    
+        
+    UINavigationController *nv = [[UINavigationController alloc]initWithRootViewController:self.tabBarController];
+    self.navigationController = nv;
+    [nv release];
+    [self.window setRootViewController:self.navigationController];
+    
+    [self.window makeKeyAndVisible];
+    
+    
+    
+    NSString *appKey = @"239725454";
+    NSString *appSecret = @"e2064ac8fab9d889a9eccecc5babad11";
+    
+    
+     sinaWeiboManager = [SinaweiboManager defaultManager];
+    [sinaWeiboManager createSinaweiboWithAppKey:appKey appSecret:appSecret appRedirectURI:kAppRedirectURI delegate:self];
+
+    
+    
+//    
+//    MySideMenueViewController *mySideMenuViewController = [[MySideMenueViewController alloc] init];
+//    
+//    MenuOptions options = MenuButtonEnabled|BackButtonEnabled;
+//
+//    
+//    // make sure to display the navigation controller before calling this
+//    [MFSideMenuManager configureWithNavigationController:self.navigationController
+//                                      sideMenuController:mySideMenuViewController
+//                                                menuSide:MenuLeftHandSide
+//                                                 options:options];
+//    [mySideMenuViewController release];
+    
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    /*
+     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     */
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    /*
+     Called when the application is about to terminate.
+     Save data if appropriate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+
+
+//for ios version below 4.2
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return [self.sinaWeiboManager.sinaweibo handleOpenURL:url] ;
+}
+
+//for ios version is or above 4.2
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    return [self.sinaWeiboManager.sinaweibo handleOpenURL:url];
+}
+
+@end
