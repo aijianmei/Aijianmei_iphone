1f7808e528f6b7aa981e5093f34bcee15118fb88
Tom Callon <tomcallon2010@gmail.com>
2013-01-11 21:03:14 +0800

    COMMITED

---
+++ SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/11/13.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface SearchTableViewController : PPTableViewController
+
+@end
