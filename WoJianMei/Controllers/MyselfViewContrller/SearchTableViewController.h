66821eb8957a1487add8620935015fade98b4c07
Tom Callon <tomcallon2010@gmail.com>
2013-01-11 21:00:04 +0800

    asdfasd

---
+++ WoJianMei/Controllers/MyselfViewContrller/SearchTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/11/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
---
+++ WoJianMei/Controllers/MyselfViewContrller/SearchTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  SearchViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/11/13.
+//
+//
+
+#import "SearchViewController.h"
+
+@interface SearchViewController ()
+
+@end
+
+@implementation SearchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
