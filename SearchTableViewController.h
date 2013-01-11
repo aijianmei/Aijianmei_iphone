3c11439806cec517ecc28f5fc75cb290ca6afbc0
Tom Callon <tomcallon2010@gmail.com>
2013-01-11 21:44:07 +0800

    ASDFAS

--- SearchTableViewController.h
+++ SearchTableViewController.h
@@ -6,8 +6,8 @@
 //
 //
 
-#import "PPTableViewController.h"
+#import <UIKit/UIKit.h>
 
-@interface SearchTableViewController : PPTableViewController
+@interface SearchTableViewController : UITableViewController
 
 @end
