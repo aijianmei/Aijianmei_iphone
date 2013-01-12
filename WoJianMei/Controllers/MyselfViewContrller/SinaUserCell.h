e71e0ba0766195ccf7055a7cd84f50569f57be13
Tom Callon <tomcallon2010@gmail.com>
2013-01-12 23:13:47 +0800

    added the cell at there .

---
+++ WoJianMei/Controllers/MyselfViewContrller/SinaUserCell.h
@@ -0,0 +1,13 @@
+//
+//  SinaUserCell.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/12/13.
+//
+//
+
+#import "PPTableViewCell.h"
+
+@interface SinaUserCell : PPTableViewCell
+
+@end
---
+++ WoJianMei/Controllers/MyselfViewContrller/SinaUserCell.m
@@ -0,0 +1,31 @@
+//
+//  SinaUserCell.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/12/13.
+//
+//
+
+#import "SinaUserCell.h"
+
+@implementation SinaUserCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
