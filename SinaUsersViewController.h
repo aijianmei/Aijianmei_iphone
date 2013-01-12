cc8a6d9e34a0ddcebb7a24d80114f9a3c5e74330
Tom Callon <tomcallon2010@gmail.com>
2013-01-12 11:12:33 +0800

    added something new by TomCallon .

---
+++ SinaUsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SinaUsersViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/12/13.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface SinaUsersViewController : PPTableViewController
+
+@end
---
+++ SinaUsersViewController.m
@@ -0,0 +1,38 @@
+//
+//  SinaUsersViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/12/13.
+//
+//
+
+#import "SinaUsersViewController.h"
+
+@interface SinaUsersViewController ()
+
+@end
+
+@implementation SinaUsersViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
