ab703e50dd672e0dcad3fb1894bd55c9aeb50b35
Tom Callon <tomcallon2010@gmail.com>
2013-01-16 19:34:38 +0800

    added some more things by Tom  Callon .

---
+++ WoJianMei/ShareViewContrller/TencentOAuthManager.h
@@ -0,0 +1,13 @@
+//
+//  TencentOAuthManager.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/16/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TencentOAuthManager : NSObject
+
+@end
---
+++ WoJianMei/ShareViewContrller/TencentOAuthManager.m
@@ -0,0 +1,13 @@
+//
+//  TencentOAuthManager.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/16/13.
+//
+//
+
+#import "TencentOAuthManager.h"
+
+@implementation TencentOAuthManager
+
+@end
