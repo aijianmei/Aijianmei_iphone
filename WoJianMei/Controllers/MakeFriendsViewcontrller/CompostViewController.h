52b54ede473c4a7dffd6451813d741a7fd96e02f
Tom Callon <tomcallon2010@gmail.com>
2013-01-21 10:54:41 +0800

    added some more things by Tom Callon .

---
+++ WoJianMei/Controllers/MakeFriendsViewcontrller/CompostViewController.h
@@ -0,0 +1,13 @@
+//
+//  CompostViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/20/13.
+//
+//
+
+#import "PPViewController.h"
+
+@interface CompostViewController : PPViewController
+
+@end
---
+++ WoJianMei/Controllers/MakeFriendsViewcontrller/CompostViewController.m
@@ -0,0 +1,38 @@
+//
+//  CompostViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/20/13.
+//
+//
+
+#import "CompostViewController.h"
+
+@interface CompostViewController ()
+
+@end
+
+@implementation CompostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
