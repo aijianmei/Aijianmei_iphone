55c476795ca80375ae0922e379fc6f49a41842cf
Tom Callon <tomcallon2010@gmail.com>
2013-03-04 12:00:32 +0800

    added the testing gittesing file .Kk

---
+++ gittesting.h
@@ -0,0 +1,13 @@
+//
+//  gittesting.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/4/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface gittesting : NSObject
+
+@end
---
+++ gittesting.m
@@ -0,0 +1,13 @@
+//
+//  gittesting.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/4/13.
+//
+//
+
+#import "gittesting.h"
+
+@implementation gittesting
+
+@end
