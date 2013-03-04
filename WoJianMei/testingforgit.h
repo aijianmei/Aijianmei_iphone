a2a2fe40690837c9c7d6b29daa19986d77b31cc8
Tom Callon <tomcallon2010@gmail.com>
2013-03-04 11:09:50 +0800

    added something for testing .

---
+++ WoJianMei/testingforgit.h
@@ -0,0 +1,13 @@
+//
+//  testingforgit.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/4/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testingforgit : NSObject
+
+@end
---
+++ WoJianMei/testingforgit.m
@@ -0,0 +1,13 @@
+//
+//  testingforgit.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/4/13.
+//
+//
+
+#import "testingforgit.h"
+
+@implementation testingforgit
+
+@end
