0c3a9d14df265fee478d12571c5b4dfc07aebb99
Tom Callon <tomcallon2010@gmail.com>
2013-03-12 17:06:06 +0800

    asdfasd

--- WoJianMei/Controllers/MoreViewController/MoreViewController.m
+++ WoJianMei/Controllers/MoreViewController/MoreViewController.m
@@ -485,6 +485,9 @@ typedef enum {
         NSInteger BUTTON_INDEX ;
         BUTTON_INDEX  = buttonIndex;
         
+        NSLog(@"This tisasfsadfd");
+        
+        
         
         switch (BUTTON_INDEX) {
             case SEND_SINA_WEIBO:
