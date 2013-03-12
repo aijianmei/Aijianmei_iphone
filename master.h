8e6d2e3170b6583b909b93289897e70980d3693c
Tom Callon <tomcallon2010@gmail.com>
2013-03-12 14:42:20 +0800

    added two files at the master branch .

---
+++ master.h
@@ -0,0 +1,13 @@
+//
+//  master.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/12/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface master : NSObject
+
+@end
---
+++ master.m
@@ -0,0 +1,13 @@
+//
+//  master.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 3/12/13.
+//
+//
+
+#import "master.h"
+
+@implementation master
+
+@end
