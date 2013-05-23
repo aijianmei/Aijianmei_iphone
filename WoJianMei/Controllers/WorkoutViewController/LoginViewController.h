9baa9aced0be04e818cd5ac0ea171415663aa243
Tom Callon <tomcallon2010@gmail.com>
2013-05-23 19:58:54 +0800

    added the login ViewController by TomCallon .

---
+++ WoJianMei/Controllers/WorkoutViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 5/23/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
