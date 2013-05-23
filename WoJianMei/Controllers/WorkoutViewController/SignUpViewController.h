3a558cd4676603d6ec6f33173abf056e8607ea42
Tom Callon <tomcallon2010@gmail.com>
2013-05-23 22:31:20 +0800

    fadsdghfjghk

---
+++ WoJianMei/Controllers/WorkoutViewController/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 5/23/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
---
+++ WoJianMei/Controllers/WorkoutViewController/SignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 5/23/13.
+//
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
