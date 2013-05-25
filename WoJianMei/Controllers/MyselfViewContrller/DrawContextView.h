6ca1e30170189e20b6d56c09c78e7d4774f6c4a2
Kaibin <ekaibwu@gmail.com>
2013-05-25 17:00:13 +0800

    modify storyboard

---
+++ WoJianMei/Controllers/MyselfViewContrller/DrawContextView.h
@@ -0,0 +1,13 @@
+//
+//  DrawContextView.h
+//  WoJianMei
+//
+//  Created by Kaibin on 13-5-25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawContextView : UIView
+
+@end
---
+++ WoJianMei/Controllers/MyselfViewContrller/DrawContextView.m
@@ -0,0 +1,31 @@
+//
+//  DrawContextView.m
+//  WoJianMei
+//
+//  Created by Kaibin on 13-5-25.
+//
+//
+
+#import "DrawContextView.h"
+
+@implementation DrawContextView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
