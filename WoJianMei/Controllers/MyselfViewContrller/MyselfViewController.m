9f4f56b09a9605157bf9c809a28349da5624e5bc
Tom Callon <tomcallon2010@gmail.com>
2013-05-25 11:37:54 +0800

    added the  three viewcontrollers

--- WoJianMei/Controllers/MyselfViewContrller/MyselfViewController.m
+++ WoJianMei/Controllers/MyselfViewContrller/MyselfViewController.m
@@ -346,16 +346,23 @@
             
         ////for testing
             
-            [self performSegueWithIdentifier:@"" sender:self];
+            [self performSegueWithIdentifier:@"WorkoutNoteViewSegue" sender:self];
             
             break;
         case 2:
             NSLog(@"I did click %@",@"标签");
+            ////for testing
+            
+            [self performSegueWithIdentifier:@"WorkoutImageViewSegue" sender:self];
+            
             break;
         case 3:
             switch (indexPath.row) {
                 case 0:
                     NSLog(@"this is %@",@"性别");
+                    ////for testing
+                    
+                    [self performSegueWithIdentifier:@"WorkoutDatasViewSegue" sender:self];
                     break;
                 case 1:
                     NSLog(@"this is %@",@"年龄");
