0403eabea694a2b161a624a81c10aabac8f5a9a7
Tom Callon <tomcallon2010@gmail.com>
2013-05-27 21:54:17 +0800

    added the uiscrollview .

---
+++ WoJianMei/Common iphone/REComposeViewController/REComposeViewController/DEComposeBackgroundScrollView.h
@@ -0,0 +1,13 @@
+//
+//  DEComposeBackgroundScrollView.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 5/27/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEComposeBackgroundScrollView : UIScrollView
+
+@end
