5560ea5ab4acefe7537fe73d39ee76f7aab5b052
Tom Callon <tomcallon2010@gmail.com>
2013-06-04 19:56:54 +0800

    added  WorkoutInfo

---
+++ WoJianMei/Controllers/MyselfViewContrller/WorkOutInfo.h
@@ -0,0 +1,13 @@
+//
+//  WorkOutInfo.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 6/4/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WorkOutInfo : NSObject
+
+@end
---
+++ WoJianMei/Controllers/MyselfViewContrller/WorkOutInfo.m
@@ -0,0 +1,13 @@
+//
+//  WorkOutInfo.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 6/4/13.
+//
+//
+
+#import "WorkOutInfo.h"
+
+@implementation WorkOutInfo
+
+@end
