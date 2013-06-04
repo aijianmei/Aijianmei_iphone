0fe1cb0d1dce4072244204628816450af407b66a
Kaibin <ekaibwu@gmail.com>
2013-06-04 23:08:52 +0800

    rm RestKit example and Test files

---
+++ WoJianMei/Article.h
@@ -0,0 +1,13 @@
+//
+//  Article.h
+//  WoJianMei
+//
+//  Created by Kaibin on 13-6-4.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Article : NSObject
+
+@end
---
+++ WoJianMei/Article.m
@@ -0,0 +1,13 @@
+//
+//  Article.m
+//  WoJianMei
+//
+//  Created by Kaibin on 13-6-4.
+//
+//
+
+#import "Article.h"
+
+@implementation Article
+
+@end
