011fd14fd7610adde2863989df087deb39b584c7
Tom Callon <tomcallon2010@gmail.com>
2013-06-27 10:17:20 +0800

    added the HomeViewController

---
+++ WoJianMei/HomeViewController.m
@@ -0,0 +1,39 @@
+
+//
+//  HomeViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 6/26/13.
+//
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
