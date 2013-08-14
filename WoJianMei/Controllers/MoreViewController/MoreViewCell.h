4b17dd6ae361c005943073ef0e7f674264fc992f
Tom Callon <tomcallon2010@gmail.com>
2013-08-14 18:27:53 +0800

    changes .

---
+++ WoJianMei/Controllers/MoreViewController/MoreViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MoreViewCell.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 8/14/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreViewCell : UITableViewCell
+
+@end
---
+++ WoJianMei/Controllers/MoreViewController/MoreViewCell.m
@@ -0,0 +1,29 @@
+//
+//  MoreViewCell.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 8/14/13.
+//
+//
+
+#import "MoreViewCell.h"
+
+@implementation MoreViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
