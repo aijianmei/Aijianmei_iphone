b74b3cac3a7a89952291c04f5006b143f6c1cb36
Tom Callon <tomcallon2010@gmail.com>
2014-01-06 17:04:13 +0800

    commits

---
+++ WoJianMei/StoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoreViewController.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreViewController : UIViewController
+
+@end
---
+++ WoJianMei/StoreViewController.m
@@ -0,0 +1,38 @@
+//
+//  StoreViewController.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/6/14.
+//
+//
+
+#import "StoreViewController.h"
+
+@interface StoreViewController ()
+
+@end
+
+@implementation StoreViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
