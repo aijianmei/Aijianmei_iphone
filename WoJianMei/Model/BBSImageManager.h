e0299bfbd23b92e77c13a661e5c475e9d7d709aa
Tom Callon <tomcallon2010@gmail.com>
2014-01-16 16:41:27 +0800

    updates .

---
+++ WoJianMei/Model/BBSImageManager.h
@@ -0,0 +1,13 @@
+//
+//  BBSImageManager.h
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/15/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBSImageManager : NSObject
+
+@end
---
+++ WoJianMei/Model/BBSImageManager.m
@@ -0,0 +1,13 @@
+//
+//  BBSImageManager.m
+//  WoJianMei
+//
+//  Created by Tom Callon  on 1/15/14.
+//
+//
+
+#import "BBSImageManager.h"
+
+@implementation BBSImageManager
+
+@end
